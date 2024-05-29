51ad312deda91c16740b65c3944e96d77cc11dce
MT <meetprakash342004@gmail.com>
2024-05-29 16:29:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./App.css";
+import Chat from "./components/Chat";
+import 'dotenv/config' 
+const URL = "https://chat-app-backend-0cyz.onrender.com"
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useState(JSON.parse(localStorage.getItem("chat-user")) || null)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`${URL}/api/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await res.json();
+    console.log(data);
+    if (data.error) {
+      throw new Error(data.error);
+    }
+
+    localStorage.setItem("chat-user", JSON.stringify(data));
+    setAuth(data)
+  };
+
+  return (
+    <>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      {!auth ? <h1>not auth</h1> : <Chat auth={auth}/>}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Chat.jsx
@@ -0,0 +1,143 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const URL = "https://chat-app-backend-0cyz.onrender.com"
+
+export default function Chat(props) {
+  const {auth} = props;  
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+  const [conversations, setConversations] = useState([]);
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      const res = await fetch(`${URL}/api/users`,{
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "jwt": localStorage.getItem('chat-user'),
+        },
+      });
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setConversations(data);
+    };
+
+    getConversations();
+  }, []);
+
+  useEffect(() => {
+    const getMessages = async () => {
+        console.log("getMessage");
+            const res = await fetch(`${URL}/api/messages/${selectedUser._id}`,{
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                  "jwt": localStorage.getItem('chat-user'),
+                },
+              });
+            const data = await res.json();
+            console.log(data);
+            if (data.error) throw new Error(data.error);
+            setMessages(data);
+        
+    };
+
+    if (selectedUser?._id) getMessages();
+}, [selectedUser?._id, setMessages]);
+
+useEffect(() => {
+    if (auth) {
+        const socket = io(URL, {
+            query: {
+                userId: auth._id,
+            },
+        });
+
+        setSocket(socket);
+
+        // // socket.on() is used to listen to the events. can be used both on client and server side
+        // socket.on("getOnlineUsers", (users) => {
+        //     setOnlineUsers(users);
+        // });
+
+        return () => socket.close();
+    } else {
+        if (socket) {
+            socket.close();
+            setSocket(null);
+        }
+    }
+}, [auth])
+
+useEffect(() => {
+    socket?.on("newMessage", (newMessage) => {
+        setMessages([...messages, newMessage]);
+    });
+
+    return () => socket?.off("newMessage");
+}, [socket, setMessages, messages]);
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+  };
+
+  const handleSendMessage = async() => {
+    const res = await fetch(`${URL}/api/messages/send/${selectedUser._id}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "jwt": localStorage.getItem('chat-user'),
+        },
+        body: JSON.stringify({ message:input }),
+    });
+    const data = await res.json();
+    if (data.error) throw new Error(data.error);
+
+    setMessages([...messages, data]);
+    setInput("");
+  };
+  return (
+    <div>
+      <h1>User</h1>
+      <div className="side-panel">
+        <h2>Users</h2>
+        <ul>
+          {conversations.map((user) => (
+            <li key={user._id} onClick={() => handleUserClick(user)}>
+              {user.fullName}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="chat-box">
+        {selectedUser ? (
+          <>
+            <h2>Chat with {selectedUser.fullName}</h2>
+            <div className="messages">
+              { messages.map((msg, index) => (
+                <div key={index} className="message">
+                  {msg.message}
+                </div>
+              ))}
+              
+            </div>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Type a message..."
+            />
+            <button onClick={handleSendMessage}>Send</button>
+          </>
+        ) : (
+          <p>Select a user to chat with</p>
+        )}
+      </div>
+    </div>
+  );
+}

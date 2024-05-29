71cae57990da3914932538425b48dad3361b0c75
MT <meetprakash342004@gmail.com>
2024-05-29 16:47:31 +0530

    inital

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import Chat from "./components/Chat";
-import 'dotenv/config' 
 const URL = "https://chat-app-backend-0cyz.onrender.com"
 
 function App() {
